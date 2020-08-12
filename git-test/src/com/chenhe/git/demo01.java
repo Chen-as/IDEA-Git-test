87188367502b0c55224494150f0c1b11c6703bbd
Chen <1767298257@qq.com>
2020-08-13 07:39:40 +0800

    提交git-test

---
+++ git-test/src/com/chenhe/git/demo01.java
@@ -0,0 +1,10 @@
+package com.chenhe.git;
+
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello demo01");
+        System.out.println("hello demo01");
+        System.out.println("hello demo01");
+        System.out.println("hello demo01");
+    }
+}
